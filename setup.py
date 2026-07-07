73c00c8c4003d311825929efe63e248078457b79
Man005-Coder <manthanrraval@gmail.com>
2026-07-07 20:45:16 +0530

    Implemented Setup.py

---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+from typing import List
+
+HYPEN_E_DOT = "-e ."
+
+def get_requirements(file_path:str)->list[str]:
+    requirement=[]
+    with open(file_path) as f:
+        requirement= f.readlines()
+        requirement=[req.replace("\n","") for req in requirement]
+
+        if  HYPEN_E_DOT in requirement:
+            requirement.remove(HYPEN_E_DOT)
+
+    return requirement
+
+setup(
+    name="First Model Package",
+    version="0.0.0",
+    author="Manthan Raval",
+    author_email="manthanrraval@gmail.com",
+    packages=find_packages(),
+    install_requires=get_requirements("requirement.txt"),
+    description="My first model package to learn new things",
+    python_requires=">=3.13.14",
+)
